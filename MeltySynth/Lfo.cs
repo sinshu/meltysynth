4916dfc19e0720d09cb58aecb6198528196e318e
sinshu <sinshu.d@gmail.com>
2021-04-13 00:24:53 +0900

    Add tests for LFO.

--- MeltySynth/Lfo.cs
+++ MeltySynth/Lfo.cs
@@ -4,11 +4,56 @@ namespace MeltySynth
 {
     internal sealed class Lfo
     {
-        internal Synthesizer synthesizer;
+        private Synthesizer synthesizer;
 
-        public Lfo(Synthesizer synthesizer)
+        private double delay;
+        private double period;
+
+        private int processedSampleCount;
+
+        private float value;
+
+        internal Lfo(Synthesizer synthesizer)
         {
             this.synthesizer = synthesizer;
         }
+
+        public void Start(float delay, float frequency)
+        {
+            this.delay = delay;
+            this.period = 1.0 / frequency;
+
+            processedSampleCount = 0;
+        }
+
+        public void Process()
+        {
+            processedSampleCount += synthesizer.BlockSize;
+
+            var currentTime = (double)processedSampleCount / synthesizer.SampleRate;
+
+            if (currentTime < delay)
+            {
+                value = 0;
+            }
+            else
+            {
+                var phase = ((currentTime - delay) % period) / period;
+                if (phase < 0.25)
+                {
+                    value = (float)(4 * phase);
+                }
+                else if (phase < 0.75)
+                {
+                    value = (float)(4 * (0.5 - phase));
+                }
+                else
+                {
+                    value = (float)(4 * (phase - 1.0));
+                }
+            }
+        }
+
+        public float Value => value;
     }
 }
--- MeltySynthTest/LfoTest_TinySoundFont.cs
+++ MeltySynthTest/LfoTest_TinySoundFont.cs
@@ -15,6 +15,44 @@ namespace MeltySynthTest
             var path = Path.Combine("ReferenceData", "TinySoundFont", "Lfo", "D03_F50.csv");
             var expected = File.ReadLines(path).Select(line => float.Parse(line.Split(',')[1])).ToArray();
             Assert.IsTrue(expected.Length == 1000);
+
+            var synthesizer = new Synthesizer(44100);
+            var lfo = new Lfo(synthesizer);
+
+            lfo.Start(0.3F, 5.0F);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                lfo.Process();
+
+                if (Math.Abs(lfo.Value - expected[i]) >= 2.5E-2)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+
+        [Test]
+        public void Lfo_D00_F70()
+        {
+            var path = Path.Combine("ReferenceData", "TinySoundFont", "Lfo", "D00_F70.csv");
+            var expected = File.ReadLines(path).Select(line => float.Parse(line.Split(',')[1])).ToArray();
+            Assert.IsTrue(expected.Length == 1000);
+
+            var synthesizer = new Synthesizer(44100);
+            var lfo = new Lfo(synthesizer);
+
+            lfo.Start(0.0F, 7.0F);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                lfo.Process();
+
+                if (Math.Abs(lfo.Value - expected[i]) >= 2.5E-2)
+                {
+                    Assert.Fail();
+                }
+            }
         }
     }
 }
