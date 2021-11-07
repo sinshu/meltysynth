477234b7d133d6d256cc51480e598cc3eb1d3b83
田中信秋 <sinshu.d@gmail.com>
2021-11-07 21:02:57 +0900

    Add Silk.NET.SDL example.

---
+++ Examples/Silk.NET.SDL/MidiPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MeltySynth;
+
+public class MidiPlayer
+{
+    private Synthesizer synthesizer;
+    private MidiFileSequencer sequencer;
+
+    private object mutex;
+
+    public MidiPlayer(string soundFontPath)
+    {
+        var settings = new SynthesizerSettings(44100);
+
+        synthesizer = new Synthesizer(soundFontPath, settings);
+        sequencer = new MidiFileSequencer(synthesizer);
+
+        mutex = new object();
+    }
+
+    public unsafe void ProcessAudio(void* data, byte* stream, int length)
+    {
+        var sampleCount = length / sizeof(float);
+        var destination = new Span<float>(stream, sampleCount);
+
+        lock (mutex)
+        {
+            sequencer.RenderInterleaved(destination);
+        }
+    }
+
+    public void Play(MidiFile midiFile, bool loop)
+    {
+        lock (mutex)
+        {
+            sequencer.Play(midiFile, loop);
+        }
+    }
+
+    public void Stop()
+    {
+        lock (mutex)
+        {
+            sequencer.Stop();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -78,6 +78,7 @@ https://www.youtube.com/watch?v=xNgsIJKxPkI
 
 * [MIDI file player for SFML.Net](https://github.com/sinshu/meltysynth/tree/main/Examples/SFML.Net)
 * [MIDI file player for Silk.NET (OpenAL)](https://github.com/sinshu/meltysynth/tree/main/Examples/Silk.NET.OpenAL)
+* [MIDI file player for Silk.NET (SDL)](https://github.com/sinshu/meltysynth/tree/main/Examples/Silk.NET.SDL)
 * [MIDI file player for OpenTK](https://github.com/sinshu/meltysynth/tree/main/Examples/OpenTK)
 * [MIDI file player for NAudio](https://github.com/sinshu/meltysynth/tree/main/Examples/NAudio)
 
