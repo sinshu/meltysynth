2462603e8d9cf86612228710a146f10d8452d846
russkyc <jrcc1023@gmail.com>
2025-03-18 10:10:00 +0800

    Add SoundFlow example

---
+++ Examples/Sound_Flow/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MeltySynth;
+using Sound_Flow;
+using SoundFlow.Backends.MiniAudio;
+using SoundFlow.Components;
+using SoundFlow.Enums;
+
+using var audioEngine = new MiniAudioEngine(44100, Capability.Playback);
+
+var sequencerSoundComponent = new SequencerSoundComponent("TimGM6mb.sf2");
+var midiFile = new MidiFile(@"C:\Windows\Media\flourish.mid");
+
+Mixer.Master.AddComponent(sequencerSoundComponent);
+
+sequencerSoundComponent.Play(midiFile, true);
+
+Console.WriteLine("Playing audio... Press any key to stop.");
+Console.ReadKey();
